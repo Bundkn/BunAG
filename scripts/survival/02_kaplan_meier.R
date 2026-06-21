5f9005a07f84f04ce33b3c18e5d81ec9e4cbca57
Bundkn <dangkimngan.vn@gmail.com>
2026-06-21 21:31:52 +0700

    Update 02_kaplan_meier.R

--- scripts/survival/02_kaplan_meier.R
+++ scripts/survival/02_kaplan_meier.R
@@ -1,1 +1,48 @@
-# kaplan meier
+# Kaplan-Meier
+install.packages("survminer")
+library(survival)
+library(survminer)
+fit <- survfit(
+  Surv(ngaydie, death) ~ agk1_group,
+  data = m
+)
+# Vẽ biểu đồ
+ggsurvplot(
+  fit,
+  data = m,
+  pval = TRUE,
+  risk.table = TRUE,
+  conf.int = TRUE,
+  xlab = "Days",
+  ylab = "Survival probability",
+  legend.title = "AGK group",
+  legend.labs = c("Low AGK", "High AGK")
+)
+# Tạo nhóm theo cutoff ROC
+m <- m %>%
+  mutate(
+    agk1_group = ifelse(
+      agk1 >= 17.4,
+      "High AGK",
+      "Low AGK"
+    )
+  )
+# Cách tìm cut-off
+# === 1. pROC
+install.packages("pROC")
+library(pROC)
+# tạo biến death
+m$death <- ifelse(m$songsot == "0", 1, 0)
+# Tạo đường cong ROC
+roc_obj <- roc(
+  response = m$death,
+  predictor = m$agk1
+)
+#Tìm điểm cắt tối ưu
+coords(
+  roc_obj,
+  "best",
+  ret = c("threshold", "sensitivity", "specificity")
+)
+
+# === 2.maxstat
